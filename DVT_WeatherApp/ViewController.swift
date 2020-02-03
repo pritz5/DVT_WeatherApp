42aa27a4ee797088c0bc655797571a55eafbec4c
Priteshsingh <priteshsinghc@gmail.com>
2020-02-03 14:31:06 +0530

    Initial Commit

---
+++ DVT_WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DVT_WeatherApp
+//
+//  Created by Priteshsingh Chandel on 03/02/20.
+//  Copyright © 2020 Priteshsingh Chandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
